b44f7427e803c3ea9c6509c31beeecf74cef9e61
rangga <ranggashadow27@gmail.com>
2022-11-12 23:13:33 +0700

    Mapping

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LearnMapping(),
+    );
+  }
+}
+
+class LearnMapping extends StatelessWidget {
+  const LearnMapping({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Learn Mapping"),
+      ),
+    );
+  }
+}
